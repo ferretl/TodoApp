7de22fb03750034fd09854c4419e20985c3f9af1
Arlie <Arlie4004@gmail.com>
2023-11-30 16:54:38 +1100

    added complete, add, remove, and update state

--- src/state.ts
+++ src/state.ts
@@ -1,26 +1,74 @@
 import {Observable ,of, range, fromEvent, zip, merge } from 'rxjs'; 
-import { last,filter,scan,map,mergeMap,take,takeUntil } from 'rxjs/operators';
-import {Todo, State, Action} from './types';
-import {RNG} from './utils';
+import {last,filter,scan,map,mergeMap,take,takeUntil } from 'rxjs/operators';
+import {Todo, State, Action, Add, Remove, Complete} from './types';
 
-export {initialState, createRNGFromSource};
-
-function createRNGFromSource <T>(source$: Observable<T>) {
-    return function createRngStream(seed: number = 0): Observable<number> {
-        const randomNumberStream = source$.pipe(
-            scan((acc:number, _: T) => RNG.hash(acc), seed)
-        );
-        return randomNumberStream;
-    };
-}
+export {initialState};
 
 
 const initialState: State = {
-    todos: []
+    todos: [],
+    currentId: 0
 };
 
 
+/**
+ * Adds a todo to the state.
+ * @param state - The current state.
+ * @param todo - The todo to be added.
+ * @returns The updated state with the new todo.
+ */
+const addTodo = (state: State, todo: Todo): State => {
+    return {
+        ...state,
+        todos: [...state.todos, todo],
+        currentId: state.currentId + 1
+    };
+}
 
+/**
+ * Removes a todo item from the state based on its id.
+ * @param state - The current state of the application.
+ * @param id - The id of the todo item to be removed.
+ * @returns The updated state with the todo item removed.
+ */
+const removeTodo = (state: State, id: number): State => {
+    return {
+        ...state,
+        todos: state.todos.filter((todo: Todo) => todo.id !== id)
+    };
+}
+
+/**
+ * Marks a todo as complete in the state.
+ * @param state - The current state of the application.
+ * @param id - The id of the todo to mark as complete.
+ * @returns The updated state with the todo marked as complete.
+ */
+const completeTodo = (state: State, id: number): State => {
+    return {
+        ...state,
+        todos: state.todos.map((todo:Todo) => {
+            return todo.id === id ? {...todo, done: true} : todo;
+        })
+    }
+}
+
+
+
+/**
+ * Updates the state based on the given action.
+ * @param state - The current state of the application.
+ * @param action - The action to be performed on the state.
+ * @returns The updated state.
+ */
 const updateState = (state: State, action: Action): State => {
-    return state;
+    if (action instanceof Add) {
+        return addTodo(state, {id: action.id, text: action.text, done: false});
+    }
+    else if (action instanceof Remove) {
+        return removeTodo(state, action.id);
+    }
+    else {
+        return completeTodo(state, action.id);
+    }
 }
