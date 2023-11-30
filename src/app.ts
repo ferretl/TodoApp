a32cfef5e17f72cc093e297796db1fc50905a55a
Arlie <Arlie4004@gmail.com>
2023-11-30 17:39:07 +1100

    cleaned up code and added documentartion

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,62 @@
-import {Observable, of, range, fromEvent, zip, merge } from 'rxjs'; 
-import { last,filter,scan,map,mergeMap,take,takeUntil } from 'rxjs/operators';
-import {Todo, State, Button, Add} from './types';
-import {initialState, updateState } from './state';
+import { Observable, of, range, fromEvent, zip, merge } from "rxjs";
+import {
+  last,
+  filter,
+  scan,
+  map,
+  mergeMap,
+  take,
+  takeUntil,
+} from "rxjs/operators";
+import { Todo, State, Button, Add } from "./types";
+import { initialState, updateState } from "./state";
 
-/** Observables */
-
+/**
+ * The main function that initializes the application.
+ */
 export function main() {
-    const buttonClick$ = fromEvent<Event>(document, 'click');
+  const buttonClick$ = fromEvent<Event>(document, "click");
 
-    const fromButton = <T>(button: Button, result: () => T) => 
+  /**
+   * Creates an observable that emits a value when a specific button is clicked.
+   *
+   * @template T The type of value emitted by the observable.
+   * @param {Button} button The button to listen for clicks on.
+   * @param {() => T} result A function that returns the value to be emitted by the observable.
+   * @returns {Observable<T>} An observable that emits a value when the button is clicked.
+   */
+  const fromButton = <T>(button: Button, result: () => T) =>
     buttonClick$.pipe(
-     filter((event: Event) => (event.target as HTMLElement).id === button),
-     map(result)
+      filter((event: Event) => (event.target as HTMLElement).id === button),
+      map(result)
     );
 
-    const getInputElement = (id: string) => document.getElementById(id) as HTMLInputElement;
+  /**
+   * Retrieves the input element with the specified id.
+   *
+   * @param {string} id - The id of the input element.
+   * @returns {HTMLInputElement} The HTMLInputElement with the specified id.
+   */
+  const getInputElement = (id: string) =>
+    document.getElementById(id) as HTMLInputElement;
 
+  const addClick$ = fromButton(
+    "addButton",
+    () => new Add(getInputElement("todoInput").value)
+  );
 
-    const addClick$ = fromButton('addButton', () => new Add(getInputElement('todoInput').value));
-
-    const source$ = merge(addClick$)
-    .pipe(
-        scan(updateState, initialState)
-    )
-     .subscribe((state: State) => {
-         console.log(state);
-        }
-    );
+  /**
+   * Observable representing the source stream.
+   * It merges the addClick$ stream and applies the scan operator with the updateState function and initialState.
+   * Subscribes to the resulting stream and logs the state to the console.
+   */
+  const source$ = merge(addClick$)
+    .pipe(scan(updateState, initialState))
+    .subscribe((state: State) => {
+      console.log(state);
+    });
 }
 
-if (typeof window !== 'undefined') {
-    main();
+if (typeof window !== "undefined") {
+  main();
 }
